0d703cf5173c338619084dd488135212ce3c1a78
Arnaud Spiwack <arnaud.spiwack@tweag.io>
2025-08-21 16:02:52 +0200

    Remove the Weave experiment
    This didn't really give me what I want. It didn't feel right. I wouldn't
    describe this experiment as successful. So I'm scrapping it.
    
    This way, I only depend on comonadic things from the comonad library.
    It's more reasonable.

--- src/Control/Monad/Indexed/Cont.hs
+++ src/Control/Monad/Indexed/Cont.hs
@@ -5,9 +5,7 @@
 module Control.Monad.Indexed.Cont where
 
 import Control.Comonad
-import Control.Comonad.Hoist.Class
 import Control.Comonad.Store
-import Control.Comonad.Weave
 import Control.Monad.Indexed qualified as Indexed
 
 newtype ContW w r r' a = ContW {runContW :: w (a -> r') -> r}
@@ -48,25 +46,8 @@ handle (ContW inner) (ContW handler) =
 pullback :: (Comonad w) => (forall x. w x -> v x) -> ContW v r r' a -> ContW w r r' a
 pullback f (ContW a) = ContW $ \wk -> a (f wk)
 
--- This isn't what we want though, we want to change the type `k` to be
--- arbitrary in the returned computation.
-once :: (Comonad w) => ContW (StoreT k w) r r' a -> ContW (StoreT k w) r r' a
-once a = handle (pullback (cohoist lower) a) empty
-
 run :: (Comonad w) => (w (a -> r) -> r) -> ContW w r r a
 run act = shift $ Indexed.pure . act
 
 run' :: (Comonad w) => (w r -> r) -> ContW w r r ()
 run' act = shift $ \wk -> Indexed.pure $ act (($ ()) <$> wk)
-
-----------------------------------------------------------------------------
---
--- Exploratory: Stacked without explicit Cont2
---
-----------------------------------------------------------------------------
-
-empty :: (Weave w r) => ContW w r r' a
-empty = ContW $ exit
-
-(<|>) :: (Weave w r) => ContW w r r' a -> ContW w r r' a -> ContW w r r' a
-(ContW a) <|> (ContW b) = ContW $ \wk -> (a `weave` b) wk
